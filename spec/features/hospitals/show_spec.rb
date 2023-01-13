55d0ffdcdda3fc19c3fac74bc1e5e5a2be5eaa08
Braxton Eddings <beddings81@gmail.com>
2023-01-13 12:31:02 -0600

    Add test for doctors ordered by patient count in hospital show page

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'hospitals show page' do
 
     @doctor1 = Doctor.create!(name:'Jawana Wetzel', specialty: 'Heart', university: 'MTSU', hospital_id: @hospital.id)
     @doctor2 = Doctor.create!(name:'Tyran Ruffin', specialty: 'Ortho', university: 'UT', hospital_id: @hospital.id)
+    @doctor3 = Doctor.create!(name:'The Good Doctor', specialty: 'Everything', university: 'Duke', hospital_id: @hospital.id)
 
     @patient1 = @doctor1.patients.create!(name: "Cynthia", age: 52)
     @patient2 = @doctor1.patients.create!(name: "Jamie", age: 62)
@@ -14,6 +15,7 @@ RSpec.describe 'hospitals show page' do
     @patient5 = @doctor2.patients.create!(name: "Stitch", age: 25)
 
     @doctor2.patients << @patient2
+    @doctor3.patients << [@patient1, @patient2, @patient3, @patient4, @patient5]
 
     visit hospital_path(@hospital)
   end
@@ -24,11 +26,11 @@ RSpec.describe 'hospitals show page' do
     within("#doctors") do
       expect(page).to have_content(@doctor1.name)
       expect(page).to have_content(@doctor2.name)
+      expect(page).to have_content(@doctor3.name)
     end
   end
 
   it 'contains a patient count for each doctor' do
-    save_and_open_page
     within("#doctor_#{@doctor1.id}") do
       expect(page).to have_content("Patients: #{@doctor1.patient_count}")
       expect(page).to have_content("Patients: 2")
@@ -37,6 +39,16 @@ RSpec.describe 'hospitals show page' do
     within("#doctor_#{@doctor2.id}") do
       expect(page).to have_content("Patients: #{@doctor2.patient_count}")
       expect(page).to have_content("Patients: 4")
+    end
+  end
+
+  it 'orders doctors by their patient count desc'  do
+    within("#doctors") do
+      expect(@doctor3.name).to appear_before(@doctor2.name)
+      expect(@doctor2.name).to appear_before(@doctor1.name)
+
+      expect(@doctor1.name).to_not appear_before(@doctor3.name)
+      expect(@doctor1.name).to_not appear_before(@doctor2.name)
     end
   end
 end
