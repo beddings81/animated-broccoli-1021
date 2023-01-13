3095d3057fdff2fe27fae91cda1452d3535825b7
Braxton Eddings <beddings81@gmail.com>
2023-01-13 12:29:50 -0600

    Add test for doctors ordered by patient count

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -14,15 +14,24 @@ RSpec.describe Doctor do
     @patient1 = @doctor1.patients.create!(name: "Cynthia", age: 52)
     @patient2 = @doctor1.patients.create!(name: "Jamie", age: 62)
     @patient3 = @doctor2.patients.create!(name: "Kileigh", age: 25)
+    @patient4 = @doctor2.patients.create!(name: "Lilo", age: 25)
+    @patient5 = @doctor2.patients.create!(name: "Stitch", age: 25)
 
     @doctor2.patients << @patient2
+  end
+  describe 'class methods' do
+    describe '#sort_by_patient_count' do
+      it 'sorts docotr by number of patients' do
+        expect(Doctor.sort_by_patient_count).to eq([@doctor2, @doctor1])
+      end
+    end
   end
 
   describe 'instance methods' do
     describe '#patient_count' do
       it 'returns the counts of patients for a doctor' do
         expect(@doctor1.patient_count).to eq(2)
-        expect(@doctor2.patient_count).to eq(2)
+        expect(@doctor2.patient_count).to eq(4)
       end
     end
   end
